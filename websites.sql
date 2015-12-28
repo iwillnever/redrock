c24cf7c6f9ecbaa0e1b1f8a78d4b7153947bde46
iwillnever <2453715092@qq.com>
2015-12-28 10:18:07 +0800

    Create websites.sql

---
+++ websites.sql
@@ -0,0 +1,14 @@
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (1, 'http://www.cqupt.edu.cn/', '重邮官网', 'campus');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (2, 'http://202.202.43.125/', '红岩网校', 'campus');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (3, 'http://jwzx.cqupt.edu.cn/', '教务在线', 'campus');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (4, 'http://222.177.140.105/default.jsp', '网上党校', 'campus');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (5, 'http://tsg.cqupt.edu.cn/', '重邮图书馆', 'study');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (6, 'http://www.cnki.net/', '中国知网', 'study');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (7, 'http://www.w3school.com.cn/', 'W3school', 'study');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (9, 'https://github.com/', 'Github', 'study');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (10, 'http://www.gaodengshuxue.cn', '高等数学', 'study');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (11, 'http://202.202.43.36/webCollege/', '文峰英语', 'study');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (14, 'http://172.22.161.11/', 'BT down', 'recreation');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (15, 'https://redrock.cqupt.edu.cn/market/', '拾货', 'recreation');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (16, 'http://vod.cqupt.edu.cn/', 'Vod 点播', 'recreation');
+INSERT INTO `websites` (`id`, `path`, `name`, `type`) VALUES (32, 'http://202.202.43.222/cxcy/index.aspx?ReturnUrl=%2fcxcy%2fmain%2fmain.aspx', '创新系统', 'campus');
